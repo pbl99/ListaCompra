3226e6e6001c56263bf2ddd813adfa276b3682b7
pbl99 <pabloam1999@hotmail.com>
2024-05-30 17:55:30 +0200

    PRIMERA PRUEBA

---
+++ spring-boot-listacompra/src/main/java/com/palmen/form/listacompra/SpringBootListacompraApplication.java
@@ -0,0 +1,13 @@
+package com.palmen.form.listacompra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootListacompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootListacompraApplication.class, args);
+	}
+
+}
---
+++ spring-boot-listacompra/src/test/java/com/palmen/form/listacompra/SpringBootListacompraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palmen.form.listacompra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootListacompraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
